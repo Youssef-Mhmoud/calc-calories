c1d146e57e3d626a4728d06aafbfdc3e24dfa2e8
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2023-06-20 19:02:34 +0300

    add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Navbar from "./components/Navbar";
+import "./App.scss";
+import { useContext } from "react";
+import { ThemeContext } from "./context/theme-context";
 
 function App() {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <>
+    <div className={`${theme ? "App dark" : "App"}`}>
       <Navbar />
-    </>
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,32 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ThemeContext } from "../context/theme-context";
 import "./Navbar.scss";
+import { useContext } from "react";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const changeTheme = () => {
+    setTheme((prevTheme) => !prevTheme);
+  };
   return (
-    <div className="navbar">
-      <h1>قم بحساب سعراتك الحرارية</h1>
-    </div>
+    <header className="header">
+      <div className="logo">
+        <h1>GYM LOGO</h1>
+      </div>
+      <nav className="navbar">
+        <ul className="navbar__links">
+          <li className="btn btn__dark-mode" onClick={changeTheme}>
+            {theme ? (
+              <FontAwesomeIcon icon={faSun} className="sun__icon" />
+            ) : (
+              <FontAwesomeIcon icon={faMoon} className="moon__icon" />
+            )}
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
